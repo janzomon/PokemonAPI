e4b2d675778355b54b97159992c0b2772e0b39e7
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-12-01 11:38:59 -0800

    Initial Commit

---
+++ PokemonAPI/PokemonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAPIApp.swift
+//  PokemonAPI
+//
+//  Created by Damon gonzalez on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonAPITests/PokemonAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAPITests.swift
+//  PokemonAPITests
+//
+//  Created by Damon gonzalez on 12/1/24.
+//
+
+import Testing
+@testable import PokemonAPI
+
+struct PokemonAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
